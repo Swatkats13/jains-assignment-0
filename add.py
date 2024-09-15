39dd3c7aeb84a55224aa5520e612b092a6c628d1
Swatkats <34835897+Swatkats13@users.noreply.github.com>
2024-09-15 13:06:40 -0400

    Assignment 0

---
+++ add.py
@@ -0,0 +1,11 @@
+def add_two_numbers(a, b):
+    return a + b
+
+number1 = 5  
+number2 = 3  
+
+
+sum_result = add_two_numbers(number1, number2)
+
+
+print(f"The sum of {number1} and {number2} is: {sum_result}")
